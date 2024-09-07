2f27361b0dae3f0bdfeb1368c1c4efd58a07a451
Derek Bruckner <dbrucknr@umich.edu>
2024-09-07 09:14:41 -0400

    Add pytest-alembic and clear some comments.

--- src/database/connect.py
+++ src/database/connect.py
@@ -1,10 +1,6 @@
 from typing import AsyncGenerator
 
-# from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.asyncio.engine import AsyncEngine
-from sqlalchemy.orm import declarative_base
-
-# from sqlalchemy.ext.asyncio.session import AsyncSession, AsyncEng
 from sqlmodel import SQLModel, create_engine
 from sqlmodel.ext.asyncio.session import AsyncSession
 
@@ -13,7 +9,6 @@ from database.config import DatabaseConfig
 config = DatabaseConfig()
 engine = create_engine(url=str(config.SQLALCHEMY_DATABASE_URI), echo=True, future=True)
 async_engine = AsyncEngine(engine)
-base = declarative_base()
 
 
 async def session() -> AsyncGenerator[AsyncSession, None]:
